ab546eb6dba5d0c3ef28604dc5c24cf0d3883027
Engees <emmawhocoded@gmail.com>
2022-04-25 22:02:33 -0800

    Started Db Setup

---
+++ database/migrations/2022_04_26_054919_create_profiles_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullname');
+            $table->string('email');
+            $table->string('contact_one');
+            $table->string('contact_two')->nullable();
+            $table->string('age_group');
+            $table->string('gender');
+            $table->string('church_location');
+            $table->string('address_one');
+            $table->string('address_two')->nullable();
+            $table->string('area');
+            $table->string('fellowship_group');
+            $table->string('friendship_centre');
+            $table->string('marital_status');
+            $table->string('birth_date');
+            $table->string('wedding_date')->nullable();
+            $table->string('occupation');
+            $table->string('leadership_position');
+            $table->string('memberId');
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
